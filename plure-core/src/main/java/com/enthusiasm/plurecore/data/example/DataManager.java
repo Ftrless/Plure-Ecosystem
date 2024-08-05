08e01334d179b931f860de1748cedb625e260d54
T0pN1ck <t0pn1ck@n0w4y.ru>
2024-08-05 18:36:10 +0300

    No longer needed

--- plure-core/src/main/java/com/enthusiasm/plurecore/data/example/DataManager.java
+++ plure-core/src/main/java/com/enthusiasm/plurecore/data/example/DataManager.java
@@ -10,17 +10,17 @@ import java.util.UUID;
 
 public class DataManager {
     public static void init() {
-        Set<DataHolderEntry> dataHolderEntries = DataService.register("warp", WarpData.class);
-
-        if (dataHolderEntries.isEmpty()) {
-            PlureCoreEntrypoint.LOGGER.info("Датахолдер пуст, добавляем новый энтри и сохраняем");
-            DataService.newDataHolder(UUID.randomUUID().toString(), WarpData.class);
-            return;
-        }
-
-        for (DataHolderEntry dataHolderEntry : dataHolderEntries) {
-            DataHolder<WarpData> dataHolder = (DataHolder<WarpData>) dataHolderEntry.dataHolder();
-            WarpData warpData = dataHolder.getDataClassDefinition();
-        }
+//        Set<DataHolderEntry> dataHolderEntries = DataService.register("warp", WarpData.class);
+//
+//        if (dataHolderEntries.isEmpty()) {
+//            PlureCoreEntrypoint.LOGGER.info("Датахолдер пуст, добавляем новый энтри и сохраняем");
+//            DataService.newDataHolder(UUID.randomUUID().toString(), WarpData.class);
+//            return;
+//        }
+//
+//        for (DataHolderEntry dataHolderEntry : dataHolderEntries) {
+//            DataHolder<WarpData> dataHolder = (DataHolder<WarpData>) dataHolderEntry.dataHolder();
+//            WarpData warpData = dataHolder.getDataClassDefinition();
+//        }
     }
 }
